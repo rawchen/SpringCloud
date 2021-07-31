7a598ff4704a3a7f7beadd6d29c99f1b1119441b
RawChen <2221999792@qq.com>
2021-07-31 22:52:04 +0800

    Eureka客户端环境

--- cloud-provider-payment8001/src/main/java/com/rawchen/springcloud/PaymentMain8001.java
+++ cloud-provider-payment8001/src/main/java/com/rawchen/springcloud/PaymentMain8001.java
@@ -2,11 +2,13 @@ package com.rawchen.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * 付款启动类
  */
 @SpringBootApplication
+@EnableEurekaClient
 public class PaymentMain8001 {
 	public static void main(String[] args) {
 		SpringApplication.run(PaymentMain8001.class, args);
