5c996dc179b6aca4b0ac139d7dbf98585b5c390a
RawChen <2221999792@qq.com>
2021-08-10 22:53:25 +0800

    服务提供者注册进Consul

---
+++ cloud-providerconsul-payment8006/src/main/java/com/rawchen/springcloud/PaymentMain8006.java
@@ -0,0 +1,17 @@
+package com.rawchen.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author RawChen
+ * @date 2021/8/10 22:47
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class PaymentMain8006 {
+	public static void main(String[] args) {
+		SpringApplication.run(PaymentMain8006.class, args);
+	}
+}
