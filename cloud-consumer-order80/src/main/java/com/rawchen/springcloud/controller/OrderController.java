280d9f3ff5f1e7a3759a200657e80d99c175c130
RawChen <2221999792@qq.com>
2021-08-11 23:22:20 +0800

    getForEntity与getForObject差别

--- cloud-consumer-order80/src/main/java/com/rawchen/springcloud/controller/OrderController.java
+++ cloud-consumer-order80/src/main/java/com/rawchen/springcloud/controller/OrderController.java
@@ -3,6 +3,7 @@ package com.rawchen.springcloud.controller;
 import com.rawchen.springcloud.entities.CommonResult;
 import com.rawchen.springcloud.entities.Payment;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -28,5 +29,16 @@ public class OrderController {
 	@GetMapping(value = "/consumer/payment/get/{id}")
 	public CommonResult<Payment> getPayment(@PathVariable("id") Long id) {
 		return restTemplate.getForObject(PAYMENT_URL + "/payment/get/" + id, CommonResult.class);
+	}
+
+	//getForEntity与getForObject差别
+	@GetMapping(value = "/consumer/payment/getForEntity/{id}")
+	public CommonResult<Payment> getPayment2(@PathVariable("id") Long id) {
+		ResponseEntity<CommonResult> entity = restTemplate.getForEntity(PAYMENT_URL + "/payment/get/" + id, CommonResult.class);
+		if (entity.getStatusCode().is2xxSuccessful()) {
+			return entity.getBody();
+		} else {
+			return new CommonResult<>(444, "操作失败");
+		}
 	}
 }
