a35e1234d866ec41488f2a88e173ce3252f79c75
RawChen <2221999792@qq.com>
2021-08-14 17:28:07 +0800

    OpenFeign服务调用

---
+++ cloud-consumer-feign-order80/src/main/java/com/rawchen/springcloud/OrderFeignMain80.java
@@ -0,0 +1,17 @@
+package com.rawchen.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+/**
+ * @author RawChen
+ * @date 2021/8/14 17:09
+ */
+@SpringBootApplication
+@EnableFeignClients
+public class OrderFeignMain80 {
+	public static void main(String[] args) {
+		SpringApplication.run(OrderFeignMain80.class, args);
+	}
+}
---
+++ cloud-consumer-feign-order80/src/main/java/com/rawchen/springcloud/service/PaymentFeignService.java
@@ -0,0 +1,20 @@
+package com.rawchen.springcloud.service;
+
+import com.rawchen.springcloud.entities.CommonResult;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+/**
+ * @author RawChen
+ * @date 2021/8/14 17:11
+ */
+@Component
+@FeignClient(value = "CLOUD-PAYMENT-SERVICE")
+public interface PaymentFeignService {
+
+	@GetMapping(value = "/payment/get/{id}")
+	public CommonResult getPaymentById(@PathVariable("id") Long id);
+
+}
