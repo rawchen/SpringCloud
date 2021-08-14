fc0b1c49bad971f516a2e401a53e8aadabd1cee0
RawChen <2221999792@qq.com>
2021-08-14 18:02:31 +0800

    OpenFeign超时控制

--- cloud-consumer-feign-order80/src/main/java/com/rawchen/springcloud/service/PaymentFeignService.java
+++ cloud-consumer-feign-order80/src/main/java/com/rawchen/springcloud/service/PaymentFeignService.java
@@ -17,4 +17,6 @@ public interface PaymentFeignService {
 	@GetMapping(value = "/payment/get/{id}")
 	public CommonResult getPaymentById(@PathVariable("id") Long id);
 
+	@GetMapping(value = "/payment/feign/timeout")
+	public String paymentFeignTimeout();
 }
