eb2ea2fba689d88ce16101590c3efa37e2819a6d
RawChen <2221999792@qq.com>
2021-08-16 23:59:15 +0800

    Hystrix之服务降级支付侧fallback

--- cloud-provider-hystrix-payment8001/src/main/java/com/rawchen/springcloud/service/PaymentService.java
+++ cloud-provider-hystrix-payment8001/src/main/java/com/rawchen/springcloud/service/PaymentService.java
@@ -1,5 +1,7 @@
 package com.rawchen.springcloud.service;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import org.springframework.stereotype.Service;
 
 import java.util.concurrent.TimeUnit;
@@ -27,13 +29,20 @@ public class PaymentService {
 	 * @param id
 	 * @return
 	 */
+	@HystrixCommand(fallbackMethod = "paymentInfo_TimeOutHandler",commandProperties = {
+			@HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "3000")
+	})
 	public String paymentInfo_TimeOut(Integer id) {
-		int timeNumber = 3;
+		int timeNumber = 5;
 		try {
 			TimeUnit.SECONDS.sleep(timeNumber);
 		} catch (InterruptedException e) {
 			e.printStackTrace();
 		}
 		return "线程池: " + Thread.currentThread().getName() + "paymentInfo_OK: " + id + "\t T 耗时(s): " + timeNumber;
+	}
+
+	public String paymentInfo_TimeOutHandler(Integer id) {
+		return "线程池: " + Thread.currentThread().getName() + "paymentInfo_TimeOutHandler: " + id + "\t T 兜底方案";
 	}
 }
