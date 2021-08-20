c76dc6c84507271b166a79319a7b3f650165c87b
RawChen <2221999792@qq.com>
2021-08-20 19:24:16 +0800

    Gateway配置常用路由断言

---
+++ cloud-gateway-gateway9527/src/test/java/Test.java
@@ -0,0 +1,12 @@
+import java.time.ZonedDateTime;
+
+/**
+ * @author RawChen
+ * @date 2021/8/20 15:46
+ */
+public class Test {
+	public static void main(String[] args) {
+		ZonedDateTime zonedDateTime = ZonedDateTime.now();
+		System.out.println(zonedDateTime);
+	}
+}
