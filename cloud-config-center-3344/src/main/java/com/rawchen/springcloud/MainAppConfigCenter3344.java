0f6b2190da2f5fd61e2e1613914d378c06648653
RawChen <2221999792@qq.com>
2021-08-21 18:34:30 +0800

    Config配置总控中心

---
+++ cloud-config-center-3344/src/main/java/com/rawchen/springcloud/MainAppConfigCenter3344.java
@@ -0,0 +1,17 @@
+package com.rawchen.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * @author RawChen
+ * @date 2021/8/20 22:50
+ */
+@SpringBootApplication
+@EnableConfigServer
+public class MainAppConfigCenter3344 {
+	public static void main(String[] args) {
+		SpringApplication.run(MainAppConfigCenter3344.class, args);
+	}
+}
