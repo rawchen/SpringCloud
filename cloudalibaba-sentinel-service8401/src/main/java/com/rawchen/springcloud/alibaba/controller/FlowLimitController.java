dc017e3c99de2263e0d71006a3f4167863301419
RawChen <2221999792@qq.com>
2021-08-28 14:15:19 +0800

    Sentinel降级之RT/异常比例/异常数

--- cloudalibaba-sentinel-service8401/src/main/java/com/rawchen/springcloud/alibaba/controller/FlowLimitController.java
+++ cloudalibaba-sentinel-service8401/src/main/java/com/rawchen/springcloud/alibaba/controller/FlowLimitController.java
@@ -3,6 +3,8 @@ package com.rawchen.springcloud.alibaba.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.concurrent.TimeUnit;
+
 /**
  * @author RawChen
  * @date 2021/8/28 12:21
@@ -24,5 +26,26 @@ public class FlowLimitController {
 	public String testB() {
 		System.out.println(Thread.currentThread().getName() + "\t" + "---testB()---");
 		return "-----testB()-----";
+	}
+
+	@GetMapping("/testD")
+	public String testD() {
+		try {
+			TimeUnit.SECONDS.sleep(1);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+//		System.out.println(Thread.currentThread().getName() + "\t" + "---testD()--- 测试RT");
+
+		System.out.println(Thread.currentThread().getName() + "\t" + "---testD()--- 测试异常比例");
+		int age = 10/0;
+		return "-----testD()-----";
+	}
+
+	@GetMapping("/testE")
+	public String testE() {
+		System.out.println(Thread.currentThread().getName() + "\t" + "---testE()--- 测试异常数");
+		int age = 10/0;
+		return "-----testE()-----";
 	}
 }
