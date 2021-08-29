bd98471dd05d5170a1678589322c23a177f956c5
RawChen <2221999792@qq.com>
2021-08-29 15:14:05 +0800

    Sentinel服务熔断fallback和blockHandler

--- cloudalibaba-consumer-nacos-order84/src/main/java/com/rawchen/springcloud/alibaba/controller/CircleBreakerController.java
+++ cloudalibaba-consumer-nacos-order84/src/main/java/com/rawchen/springcloud/alibaba/controller/CircleBreakerController.java
@@ -1,6 +1,7 @@
 package com.rawchen.springcloud.alibaba.controller;
 
 import com.alibaba.csp.sentinel.annotation.SentinelResource;
+import com.alibaba.csp.sentinel.slots.block.BlockException;
 import com.rawchen.springcloud.entities.CommonResult;
 import com.rawchen.springcloud.entities.Payment;
 import lombok.extern.slf4j.Slf4j;
@@ -25,7 +26,10 @@ public class CircleBreakerController {
 	private RestTemplate restTemplate;
 
 	@RequestMapping("/consumer/fallback/{id}")
-	@SentinelResource(value = "fallback") //没有配置
+//	@SentinelResource(value = "fallback") //没有配置
+//	@SentinelResource(value = "fallback", fallback = "handlerFallback") //fallback只负责业务异常
+//	@SentinelResource(value = "fallback", blockHandler = "blockHandler") //blockHandler只负责sentinel控制台配置违规
+	@SentinelResource(value = "fallback", fallback = "handlerFallback", blockHandler = "blockHandler") //blockHandler只负责sentinel控制台配置违规
 	public CommonResult<Payment> fallback(@PathVariable Long id) {
 		CommonResult<Payment> result = restTemplate.getForObject(
 				SERVICE_URL + "/paymentSQL/" + id, CommonResult.class, id);
@@ -37,5 +41,16 @@ public class CircleBreakerController {
 		}
 
 		return result;
+	}
+	//本例是fallback
+	public CommonResult handlerFallback(@PathVariable Long id, Throwable e) {
+		Payment payment = new Payment(id, "null");
+		return new CommonResult<>(444, "兜底异常handlerFallback，exception内容：" + e.getMessage(), payment);
+	}
+
+	//本例是blockHandler
+	public CommonResult blockHandler(@PathVariable Long id, BlockException e) {
+		Payment payment = new Payment(id, "null");
+		return new CommonResult<>(445, "blockHandler-sentinel限流，无此流水：blockException " + e.getMessage(), payment);
 	}
 }
